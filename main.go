8ca8f8fc7b6c5ff8e40d90737306423152c0b67b
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-07-14 16:14:35 +0900

    build: upgrade version string

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "gria"
-	app.Version = "0.1.1"
+	app.Version = "0.1.2"
 	app.Author = "suzuki-shunsuke https://github.com/suzuki-shunsuke"
 	app.Usage = "generate test function's scaffold for golang"
 	app.Action = command.Gen
